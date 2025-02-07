5e74f35a8c9b194f09daaa8b7b4ee5ad88aefa37
samrawit <samri.berih@gmail.com>
2025-02-07 14:36:40 -0800

    last

--- routes/index.js
+++ routes/index.js
@@ -16,6 +16,30 @@ router.use("/api/session", sessionRouter);
 //   res.send('Hello World!');
 // });
 
+// Static routes
+// Serve React build files in production
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path');
+  // Serve the frontend's index.html file at the root route
+  router.get('/', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
+    );
+  });
+
+  // Serve the static assets in the frontend's build folder
+  router.use(express.static(path.resolve("../frontend/dist")));
+
+  // Serve the frontend's index.html file at all other routes NOT starting with /api
+  router.get(/^(?!\/?api).*/, (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
+    );
+  });
+}
+
 // backend/routes/index.js
 // ...
 // Add a XSRF-TOKEN cookie
